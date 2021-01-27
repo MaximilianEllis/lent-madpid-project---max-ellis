70e2a716c7c1fba7ff057c4aba382700ace9b43d
MaximilianEllis <77404094+MaximilianEllis@users.noreply.github.com>
2021-01-27 16:38:46 +0000

    Update main.ts, images.g.jres, images.g.ts

--- main.ts
+++ main.ts
@@ -1,1 +1,47 @@
-
+sprites.onOverlap(SpriteKind.Player, SpriteKind.Food, function (sprite, otherSprite) {
+    info.changeScoreBy(1)
+})
+let food2: Sprite = null
+let mySprite2 = sprites.create(img`
+    . . . . . . . . . . . . . . . . 
+    . . . . . f f f f f f . . . . . 
+    . . . . f f f f f f f f . . . . 
+    . . . f f f f f f f f f . . . . 
+    . . . f f f f f f f f f . . . . 
+    . . . f f f f f f f f f . . . . 
+    . . . f f f f f f f f f . . . . 
+    . . . . f f f f f f f . . . . . 
+    . . . . . . f f f . . . . . . . 
+    . . . f f f f f f f f . . . . . 
+    . . f f f f f f f f f f f . . . 
+    . . . . . . f f f . . . f . . . 
+    . . . . . f f f f f . . . . . . 
+    . . . . f f f f f f . . . . . . 
+    . . . . f f . . f f f . . . . . 
+    . . . . f f . . . f f . . . . . 
+    `, SpriteKind.Player)
+mySprite2.setPosition(65, 52)
+scene.setBackgroundColor(1)
+controller.moveSprite(mySprite2, 100, 100)
+mySprite2.setStayInScreen(true)
+game.onUpdateInterval(5000, function () {
+    food2 = sprites.create(img`
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . f f f . . . . . . . 
+        . . . . . . f f f f f . . . . . 
+        . . . . . f f f f f f . . . . . 
+        . . . . . f f f f f f . . . . . 
+        . . . . . . f f f f . . . . . . 
+        . . . . . . . f f . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        . . . . . . . . . . . . . . . . 
+        `, SpriteKind.Food)
+    food2.setPosition(randint(0, 100), randint(0, 100))
+})
